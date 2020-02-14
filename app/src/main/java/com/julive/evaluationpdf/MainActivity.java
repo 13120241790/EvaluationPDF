5c22d6ccc44e89cc867c42f9cd620c3a5ce4ba0b
zhouxuming <zhouxuming@rongcloud.cn>
2020-02-14 15:01:31 +0800

    add pdf url

--- app/src/main/java/com/julive/evaluationpdf/MainActivity.java
+++ app/src/main/java/com/julive/evaluationpdf/MainActivity.java
@@ -7,6 +7,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    //个人服务器上的 pdf 测试资源可能随着服务器不续费停机地址失效
+    public static final String PDF_URL = "http://106.13.169.38/test.pdf";
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
