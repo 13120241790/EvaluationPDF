9926bffec082fa404091a4369c728097a727dc63
zhouxuming <zhouxuming@rongcloud.cn>
2020-02-14 18:46:26 +0800

    本地 url 路径

--- app/src/main/java/com/julive/evaluationpdf/MainActivity.java
+++ app/src/main/java/com/julive/evaluationpdf/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     //个人服务器上的 pdf 测试资源可能随着服务器不续费停机地址失效
     public static final String PDF_URL = "http://106.13.169.38/test.pdf";
 
+    public static final String LOCAL_FILE = "file:///android_asset/test.pdf";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
