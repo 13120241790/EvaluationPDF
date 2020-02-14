d236ab993b3bdb3db2a4ce89c5b21d2fa7aaa873
zhouxuming <zhouxuming@rongcloud.cn>
2020-02-14 19:08:47 +0800

    pdf js local

--- app/src/main/java/com/julive/evaluationpdf/MainActivity.java
+++ app/src/main/java/com/julive/evaluationpdf/MainActivity.java
@@ -1,6 +1,9 @@
 package com.julive.evaluationpdf;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +15,25 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String LOCAL_FILE = "file:///android_asset/test.pdf";
 
+    WebView mWebView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+        mWebView.loadUrl("file:///android_asset/index.html?" + LOCAL_FILE);
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void initView() {
+        mWebView = findViewById(R.id.webView);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
     }
 }
